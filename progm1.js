10808f2cd5c53efc21f795c6f93affd1dfafc254
Sushmita Tripathi <110046285+Sushmitacode12@users.noreply.github.com>
2022-11-06 13:39:41 +0530

    Add files via upload

---
+++ progm1.js
@@ -0,0 +1,7 @@
+var obj = {num:2};
+
+var add = function(a){
+    return this.num+a;
+};
+
+console.log(add.call(obj,3));
---
+++ program2.js
@@ -0,0 +1,8 @@
+var obj ={num:2};
+ var add = function(a,b,c){
+    return this.num + a+b+c;
+ };
+
+ var arr = [1,2,3];
+
+ console.log(add.apply(obj,arr));
